c4a8addd3b2e35a4b472d08c9a0d2ff80dc0a86f
ChloeBezault <chloe.b@outlook.pt>
2022-04-28 22:48:01 +0100

    Implementação de todo o HTML da Homepage

--- src/App.js
+++ src/App.js
@@ -1,35 +1,40 @@
 import './App.css';
+import './styles/Header.css';
+import './styles/Homepage.css';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Navbar from "./Components/navbar/Navbar";
+import Header from "./Components/header/Header";
 import Homepage from './Components/homepage/Homepage';
 import Footer from './Components/footer/Footer';
 
-import leTilleul from './Components/navbar/LeTilleul';
-import Réceptions from './Components/navbar/Réceptions';
-import DédiéauxProfessionnels from './Components/navbar/DédiéauxProfessionnels';
-import AubergeDuTilleul from './Components/navbar/AubergeDuTilleul';
-import Portfolio from './Components/navbar/Portfolio';
-import ContactRéservations from './Components/navbar/ContactRéservations';
+import leTilleul from './Components/header/leTilleul';
+import Réceptions from './Components/header/Réceptions';
+import DédiéauxProfessionnels from './Components/header/DédiéauxProfessionnels';
+import AubergeDuTilleul from './Components/header/AubergeDuTilleul';
+import Portfolio from './Components/header/Portfolio';
+import ContactRéservations from './Components/header/ContactRéservations';
 
 function App() {
   return (
+    
     <BrowserRouter>
       <div className="App">
-        <Navbar/>
-        <Routes>
-          <Route path="/leTilleul" element={<leTilleul />} />
-          <Route path="/receptions" element={<Réceptions />} />
-          <Route path="/forProfessionals" element={<DédiéauxProfessionnels />} />
-          <Route index path="/" element={<AubergeDuTilleul />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<ContactRéservations />} />
-        </Routes>
-        <Homepage/>
-        <Footer/>
+          <Header/>
+          <Routes>
+            <Route index path="/" element={<AubergeDuTilleul />} />
+            <Route path="/leTilleul" element={<leTilleul />} />
+            <Route path="/receptions" element={<Réceptions />} />
+            <Route path="/forProfessionals" element={<DédiéauxProfessionnels />} />
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/contact" element={<ContactRéservations />} />
+          </Routes>
+          <Homepage/>
+          <Footer/>
       </div>
     </BrowserRouter>
+      
+   
   );
 }
 
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -2,13 +2,13 @@ import { NavLink } from "react-router-dom";
 
 export default function Footer() {
     return (
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to="/leTilleul">FOOTER</NavLink>
-                </li>
-            </ul>
-        </nav>
+        <footer>
+            <nav>
+
+                
+            </nav>
+
+        </footer>
     )
 
 }
