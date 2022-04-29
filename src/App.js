f072d76e95d9f3800c38c8323abfbb59d73b0c5f
ChloeBezault <chloe.b@outlook.pt>
2022-04-29 17:16:25 +0100

    Implementação do HTML no Footer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,19 @@
 import './App.css';
 import './styles/Header.css';
 import './styles/Homepage.css';
+import './styles/Footer.css';
 
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Header from "./Components/header/Header";
-import Homepage from './Components/homepage/Homepage';
-import Footer from './Components/footer/Footer';
+import Header from "./Components/SameWebsite/Header";
+import Homepage from './Components/SameWebsite/Homepage';
+import Footer from './Components/SameWebsite/Footer';
 
-import leTilleul from './Components/header/leTilleul';
-import Réceptions from './Components/header/Réceptions';
-import DédiéauxProfessionnels from './Components/header/DédiéauxProfessionnels';
-import AubergeDuTilleul from './Components/header/AubergeDuTilleul';
-import Portfolio from './Components/header/Portfolio';
-import ContactRéservations from './Components/header/ContactRéservations';
+import leTilleul from './Components/Navigation/leTilleul';
+import Réceptions from './Components/Navigation/Réceptions';
+import DédiéauxProfessionnels from './Components/Navigation/DédiéauxProfessionnels';
+import Portfolio from './Components/Navigation/Portfolio';
+import Contact from './Components/Navigation/Contact';
 
 function App() {
   return (
@@ -22,12 +22,11 @@ function App() {
       <div className="App">
           <Header/>
           <Routes>
-            <Route index path="/" element={<AubergeDuTilleul />} />
-            <Route path="/leTilleul" element={<leTilleul />} />
+            <Route path="/letilleul" element={<leTilleul />} />
             <Route path="/receptions" element={<Réceptions />} />
             <Route path="/forProfessionals" element={<DédiéauxProfessionnels />} />
             <Route path="/portfolio" element={<Portfolio />} />
-            <Route path="/contact" element={<ContactRéservations />} />
+            <Route path="/contact" element={<Contact />} />
           </Routes>
           <Homepage/>
           <Footer/>
