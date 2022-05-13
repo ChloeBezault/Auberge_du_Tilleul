2d706866c4903319d04374bf803fcb8eb7eb14fd
ChloeBezault <chloe.b@outlook.pt>
2022-05-13 17:18:08 +0100

    Concretização da página Notre Cuisine com grid para album de fotografias e com a preocupação de ser responsive

--- src/components/Pages/NotreCuisine.jsx
+++ src/components/Pages/NotreCuisine.jsx
@@ -1,7 +1,36 @@
 import { NavLink } from "react-router-dom";
 
 
-export default function NotreCuisine() {
+function NotreCuisine() {
+
+    const GalleryCuisine= [
+        {src:"/images/Img-NotreCuisine/1.jpg", id:"1"},
+        {src:"/images/Img-NotreCuisine/2.jpg", id:"2"},
+        {src:"/images/Img-NotreCuisine/3.jpg", id:"3"},
+        {src:"/images/Img-NotreCuisine/4.jpg", id:"4"},
+        {src:"/images/Img-NotreCuisine/5.jpg", id:"5"},
+        {src:"/images/Img-NotreCuisine/6.jpeg", id:"6"},
+        {src:"/images/Img-NotreCuisine/7.jpg", id:"7"},
+        { src:"/images/Img-NotreCuisine/8.jpg", id:"8"},
+        { src:"/images/Img-NotreCuisine/9.jpg", id:"9"},
+        { src:"/images/Img-NotreCuisine/10.jpg", id:"10"},
+        {src:"/images/Img-NotreCuisine/11.jpeg", id:"11"},
+        {src:"/images/Img-NotreCuisine/12.jpg", id:"12"},
+        {src:"/images/Img-NotreCuisine/13.jpg", id:"13"},
+        {src:"/images/Img-NotreCuisine/14.jpg", id:"14"},
+        { src:"/images/Img-NotreCuisine/15.jpg", id:"15"},
+        { src:"/images/Img-NotreCuisine/16.jpg", id:"16"},
+        { src:"/images/Img-NotreCuisine/17.jpg", id:"17"},
+        { src:"/images/Img-NotreCuisine/18.jpg", id:"18"},
+        { src:"/images/Img-NotreCuisine/19.jpg", id:"19"},
+        { src:"/images/Img-NotreCuisine/20.jpg", id:"20"},
+        { src:"/images/Img-NotreCuisine/21.jpg", id:"21"},
+        { src:"/images/Img-NotreCuisine/22.jpg", id:"22"},
+        { src:"/images/Img-NotreCuisine/23.jpg", id:"23"},
+        { src:"/images/Img-NotreCuisine/24.jpg", id:"24"},
+    ]
+
+    
     return (
         <main className="notreCuisine">
             <section className="CuisineApresentation">
@@ -10,74 +39,25 @@ export default function NotreCuisine() {
                 </h2>
                 <div className="cuisine_introduction">
                     <p>Notre Chef et son équipe proposent une cuisine de saison élaborée à partir de produits frais sélectionnés pour leur qualité.</p>
-                    <p>Depuis 30 ans nous nous efforçons d'imaginer une cuisine moderne, géneéreuse et originale.</p>
+                    <p>Depuis 30 ans nous nous efforçons d'imaginer une cuisine moderne, généreuse et originale.</p>
                     <p>Nous nous adaptons pour que vos événements soient au plus près de vos attentes.</p>
                 </div>
             </section>
             <section className="notreCuisine_Album">
-                <div className="gallery">
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/1.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/2.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/3.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/4.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/5.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/6.jpeg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/7.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                    <img src="/images/Img-NotreCuisine/8.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                    <img src="/images/Img-NotreCuisine/9.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/10.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/11.jpeg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/12.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/13.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/14.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/15.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/16.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/17.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/18.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                    <figure className="gallery_item">
-                        <img src="/images/Img-NotreCuisine/19.jpg" className="galleryImg" alt="" width="300"></img>
-                    </figure>
-                </div>
+                
+                <div className="galleryContainer">
+                    { GalleryCuisine.map((item, i) => {
+                    return (
+                        <div key={i} className="galleryItem">
+                            <img src={item.src} id={item.id} className={item.className}alt="ImagesFood"/>
+                        </div> 
+                    )}
+                    )}
+                   
+                </div>    
             </section>
-
-
         </main>
-
     );
 }
+
+export default NotreCuisine;
